ba09bdb88d77aa4a43912add14f5c7a0f8007200
wscott <wscott0318@gmail.com>
2021-12-21 04:31:11 +0300

    chess room prototype

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Workflow
-- Create the new branch from dev per task or updates
-- Create Draft Pull Request if in progress
-- After completing the task, please merge the recent dev branch and assign me to review
-- After feedback, please merge the recent dev branch and create a pull request
-- Please don't forget this.
+# LucidLands Chess Game
---
+++ src/utils/constant.js
@@ -0,0 +1,58 @@
+export const cameraProps = {
+    fov: 75,
+    aspect: window.innerWidth / window.innerHeight,
+    near: 0.1,
+    far: 1000,
+    position: {
+        x: 0,
+        y: 8,
+        z: 5,
+    }
+}
+
+export const ambientLightProps = {
+    color: 0xffffff,
+    intensity: 1,
+}
+
+export const spotLightProps = {
+    color: 0xffffff,
+    intensity: 0.3,
+    position: {
+        x: 100,
+        y: 1000,
+        z: 100,
+    },
+    castShadow: true,
+    shadow: {
+        mapSize: {
+            width: 1024,
+            height: 1024,
+        },
+        camera: {
+            near: 0.1,
+            far: 1000,
+            fov: 30,
+        }
+    }
+}
+
+export const alphaBet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
+export const tileSize = 1;
+
+export const lightTone = '#f7e58f';
+export const darkTone = '#123';
+
+export const boardSize = 8;
+
+export const modelProps = {
+    'board': {
+        scale: 0.8,
+        position: {
+            x: 0.45,
+            y: 0,
+            z: -0.65,
+        }
+    }
+}
