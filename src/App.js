3520b8fce955a06a19479c4d46f8317ecd854a04
dmitrii <dmitrii.diachuk2020@gmail.com>
2021-12-23 13:07:18 -0600

    add 2 popup screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import Scene from './components/Scene/Scene';
+import "./App.css";
+import Scene from "./components/Scene/Scene";
+import Logo from "./components/UI/Logo"
+import Popup from "./components/UI/Popup/Popup";
+import {useState} from 'react'
 
-import { Game, move, status, moves, aiMove, getFen } from 'js-chess-engine'
+import { Game, move, status, moves, aiMove, getFen } from "js-chess-engine";
 const game = new Game();
 
 function App() {
+  const [modalShow, setModalShow] = useState(true);
+
   return (
     <div className="App">
-      <Scene game={game} />
+      {/* <Logo show={modalShow} onHide={() => setModalShow(false)}></Logo> */}
+      <Popup show={modalShow} onHide={() => setModalShow(false)}></Popup>
+      {/* <Scene game={game} /> */}
     </div>
   );
 }
---
+++ src/components/UI/Popup/Popup.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../ui_common.css";
+import "./Popup.scss";
+
+export default class Popup extends Component {
+  render() {
+    return (
+      <Modal
+        className="Popup"
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <div className="form">
+          <div className="content">
+            and need a popup window (in general) to show information, example :
+            confirm if user quit the game, deposit money...
+          </div>
+          <div className="footer">
+            <button className="button">Quit</button>
+            <button className="button">Deposit</button>
+          </div>
+        </div>
+      </Modal>
+    );
+  }
+}
