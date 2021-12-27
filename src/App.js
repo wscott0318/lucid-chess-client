7e69ed426ba07763b69b42cb0c683e789aa2c49e
solution-pool <artur.agababian2020@gmail.com>
2021-12-27 18:08:20 +0300

    rooms responsive fix

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
     <div className="App">
       {/* <Logo show={modalShow} onHide={() => setModalShow(false)}></Logo> */}
       {/* <Popup show={modalShow} onHide={() => setModalShow(false)}></Popup> */}
-      {/* <RoomsModal show={modalShow} onHide={() => setModalShow(false)}></RoomsModal> */}
-      <PawnModal show={modalShow} onHide={() => setModalShow(false)} />
+      <RoomsModal show={modalShow} onHide={() => setModalShow(false)}></RoomsModal>
+      {/* <PawnModal show={modalShow} onHide={() => setModalShow(false)} /> */}
       {/* <Scene game={game} /> */}
     </div>
   );
