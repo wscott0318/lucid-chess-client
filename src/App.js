6e831419482f93cbe3f381c54d0c5512c184be95
dmitrii <dmitrii.diachuk2020@gmail.com>
2021-12-27 02:03:33 -0600

    complete design

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Logo from "./components/UI/Logo/Logo"
 import Popup from "./components/UI/Popup/Popup";
 import Victory from "./components/UI/Victory/Victory";
 import Level from "./components/UI/Level/Level";
-import JoinGame from "./components/UI/JoinGame/JoinGame";
+import GameSelect from "./components/UI/GameSelect/GameSelect";
 import {useState} from 'react'
 import "./components/UI/ui_common.css";
-// import Play2Earn from "./components/Play2Earn/Play2Earn";
-// import InviteFriend from "./components/InviteFriend/InviteFriend";
-// import CreateGame from "./components/CreateGame/CreateGame";
-// import Loading from "./components/Loading/Loading";
+import Play2Earn from "./components/Play2Earn/Play2Earn";
+import InviteFriend from "./components/InviteFriend/InviteFriend";
+import CreateGame from "./components/CreateGame/CreateGame";
+import Loading from "./components/Loading/Loading";
 
 import { Game, move, status, moves, aiMove, getFen } from "js-chess-engine";
 const game = new Game();
@@ -27,7 +27,8 @@ function App() {
       {/* <Logo show={modalShow} onHide={() => setModalShow(false)}></Logo> */}
       {/* <Popup show={modalShow} onHide={() => setModalShow(false)}></Popup> */}
       {/* <Victory show={modalShow} onHide={() => setModalShow(false)}></Victory> */}
-      <Level show={modalShow} onHide={() => setModalShow(false)}></Level>
+      {/* <Level show={modalShow} onHide={() => setModalShow(false)}></Level> */}
+      <GameSelect show={modalShow} onHide={() => setModalShow(false)}></GameSelect>
       {/* <Scene game={game} /> */}
     </div>
   );
