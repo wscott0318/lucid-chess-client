1b683592dbc44eb8892319db9562211acaa59d39
dmitrii <dmitrii.diachuk2020@gmail.com>
2021-12-28 01:01:26 -0600

    add game state UI and align items to center

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GameSelect from "./components/UI/GameSelect/GameSelect";
 import Level from "./components/UI/Level/Level";
-import FriendPlay from './views/FriendPlay';
-import GameScene from './views/GameScene';
+import FriendPlay from "./views/FriendPlay";
+import GameScene from "./views/GameScene";
 import MatchPlay from "./views/MatchPlay";
 
+// import GameState from "./components/UI/GameState/GameState";
+
 function App() {
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<Routes>
-					<Route path="/" element={<GameSelect />} />
-					<Route path="/matchPlay" element={<MatchPlay />} />
-					<Route path="/friendPlay/*" element={<FriendPlay />} />
-					<Route path="/machinePlay" element={<Level />}  />
-					<Route path="/gameScene" element={<GameScene />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+    	<div className="App">
+    		<Routes>
+    			<Route path="/" element={<GameSelect />} />
+    			<Route path="/matchPlay" element={<MatchPlay />} />
+    			<Route path="/friendPlay/*" element={<FriendPlay />} />
+    			<Route path="/machinePlay" element={<Level />}  />
+    			<Route path="/gameScene" element={<GameScene />} />
+    		</Routes>
+    	</div>
+    </BrowserRouter>
+
+    // <GameState></GameState>
+  );
 }
 
 export default App;
