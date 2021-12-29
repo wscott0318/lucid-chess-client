eba4164b92aaf093877d91dfe3fa38729d50fef9
dmitrii <dmitrii.diachuk2020@gmail.com>
2021-12-29 05:36:06 -0600

    update game scene state UI - to top

--- src/components/UI/GameState/GameStateFooter.js
+++ src/components/UI/GameState/GameStateFooter.js
@@ -3,12 +3,13 @@ import "./GameStateFooter.scss";
 export const GameStateFooter = () => {
   return (
     <div className="GameStateFooter">
-      <div className="u-footer">
-        <div className="u-back"></div>
-        <div className="u-footer-right">
-          <div className="u-undo"></div>
-          <div className="u-setting"></div>
-        </div>
+      <div className="u-back"></div>
+      <div className="u-footer-right">
+        <div className="u-undo"></div>
+        <div className="u-setting"></div>
+        <div className="u-left-volcano"></div>
+        <div className="u-right-volcano"></div>
+        <div className="u-fore-volcano"></div>
       </div>
     </div>
   );
