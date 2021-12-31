4bd7c755551996b40be090ea028b84e5bcb2eb7b
dmitrii <dmitrii.diachuk2020@gmail.com>
2021-12-30 18:45:30 -0600

    connect wallet prototype

--- src/App.js
+++ src/App.js
@@ -6,24 +6,22 @@ import Level from "./components/UI/Level/Level";
 import FriendPlay from "./views/FriendPlay";
 import GameScene from "./views/GameScene";
 import MatchPlay from "./views/MatchPlay";
-
-// import GameState from "./components/UI/GameState/GameState";
+import Connect from "./components/UI/Connect/Connect";
 
 function App() {
   return (
     <BrowserRouter>
-    	<div className="App">
-    		<Routes>
-    			<Route path="/" element={<GameSelect />} />
-    			<Route path="/matchPlay" element={<MatchPlay />} />
-    			<Route path="/friendPlay/*" element={<FriendPlay />} />
-    			<Route path="/machinePlay" element={<Level />}  />
-    			<Route path="/gameScene" element={<GameScene />} />
-    		</Routes>
-    	</div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<GameSelect />} />
+          <Route path="/matchPlay" element={<MatchPlay />} />
+          <Route path="/friendPlay/*" element={<FriendPlay />} />
+          <Route path="/machinePlay" element={<Level />} />
+          <Route path="/gameScene" element={<GameScene />} />
+          <Route path="/connect" element={<Connect show={true} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-
-    // <GameState></GameState>
   );
 }
 
---
+++ src/components/UI/Connect/Connect.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Connect.scss";
+
+const arrInfo = {
+  'connect': {
+    text: "Your need ...",
+    button: "Connect Wallet",
+  },
+  'join': {
+    text: "Your need1 ...",
+    button: "Connect Wallet1",
+  },
+  'deposit': {
+    text: "Your need2 ...",
+    button: "Connect Wallet2",
+  },
+};
+
+export const Connect = ({ stageProp }) => {
+  const [stage, setStage] = useState(stageProp);
+
+  useEffect(() => {
+		if (!stage) setStage("connect");
+  }, []);
+
+
+  const nextStage = () => {
+    console.log(stage);
+    switch (stage) {
+      case "connect":
+        setStage("join");
+        break;
+      case "join":
+        setStage("deposit");
+        break;
+      case "deposit":
+        break;
+    }
+  };
+
+  return (
+    <div className="Connect">
+      <div className="u-container">
+        <div className="u-ribbon">Play to earn</div>
+        <div className="u-content">
+          <div className="u-content-container">
+            <div className="u-text">Text</div>
+						<div className="u-button" onClick={() => nextStage()}>
+							Button
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Connect;
