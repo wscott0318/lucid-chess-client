f046aee914b5047fdb2944e7d2d8cfc778b2b8a4
dmitrii <dmitrii.diachuk2020@gmail.com>
2022-01-01 17:06:36 -0600

    add refund modal in scene.

--- src/components/UI/Loading/Loading.js
+++ src/components/UI/Loading/Loading.js
@@ -1,36 +1,47 @@
-import './Loading.scss';
+import "./Loading.scss";
 
-import star_full from '../../../assets/img/star.png'
-import star_none from '../../../assets/img/star_none.png';
-import { useEffect, useRef, useState } from 'react';
+import star_full from "../../../assets/img/star.png";
+import star_none from "../../../assets/img/star_none.png";
+import { useEffect, useRef, useState } from "react";
+import Refund from "../Refund/Refund";
 
 const starArray = [0, 1, 2, 3, 4, 5];
 
 export const Loading = ({ title }) => {
-    const [currentTime, setCurrentTime] = useState(0);
-    const timeInterval = useRef(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [refund, setRefund] = useState(false);
+  const timeInterval = useRef(0);
 
-    useEffect(() => {
-        timeInterval.current = setInterval(() => setCurrentTime(prev => prev + 1), 1000);
+  useEffect(() => {
+    timeInterval.current = setInterval(() => {
+      setCurrentTime((prev) => prev + 1);
+      if (currentTime > 10) setRefund(true);
+    }, 1000);
 
-        return () => clearInterval(timeInterval.current)
-    });
+    return () => clearInterval(timeInterval.current);
+  });
 
-    return (
-        <div className='loading'>
-            <div className='loading__container' >
-                <div className = "loading__stars" >
-                    {
-                        starArray.map((item, idx) => (
-                            <img key={`star_${idx}`} className = "star" src= { (idx <= currentTime % starArray.length) ? star_full : star_none} alt="pic" />
-                        ))
-                    }
-                </div>
-                <div className = "loading__title" >
-                    <p>{ title }</p>
-                </div>
-            </div>
+  return (
+    <div className="loading">
+      <div className="loading__container">
+        <div className="loading__stars">
+          {starArray.map((item, idx) => (
+            <img
+              key={`star_${idx}`}
+              className="star"
+              src={
+                idx <= currentTime % starArray.length ? star_full : star_none
+              }
+              alt="pic"
+            />
+          ))}
         </div>
-    )
-}
+        <div className="loading__title">
+          <p>{title}</p>
+        </div>
+      </div>
+      <Refund show={refund} msg={"Can't load a room!"}></Refund>
+    </div>
+  );
+};
 export default Loading;
