c1ef6328e18e9d70dcf5f74a7db93f8b4dbe2cec
wscott <wscott0318@gmail.com>
2022-01-04 05:16:07 +0300

    change draw icon

--- src/components/UI/GameState/GameStateFooter.js
+++ src/components/UI/GameState/GameStateFooter.js
@@ -7,7 +7,7 @@ export const GameStateFooter = ({quitAction, showInventoryAction, sendDrawReques
       <div className="u-back" onClick={ showInventoryAction }></div>
       <div className="u-footer-right">
         <div className="u-undo" onClick={quitAction}></div>
-        <div className="u-setting" onClick={ sendDrawRequest }></div>
+        <div className="u-draw" onClick={ sendDrawRequest }></div>
       </div>
     </div>
   );
